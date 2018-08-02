8b98148b68b3aa6f96aa4272df132da0dc691938
alonshp <alonshp1@gmail.com>
2018-08-02 16:38:14 +0300

    article page

--- app/src/main/java/com/example/alonshprung/outbrainv2/SmartFeedAdapter.java
+++ app/src/main/java/com/example/alonshprung/outbrainv2/SmartFeedAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.outbrain.OBSDK.Outbrain;
 import com.outbrain.OBSDK.SmartFeed.OBSmartFeed;
@@ -14,10 +15,12 @@ import com.outbrain.OBSDK.Viewability.OBTextView;
 public class SmartFeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private OBSmartFeed obSmartFeed;
-    private static final int ORIGINAL_RECYCLE_VIEW_SIZE = 1;
-//    private static final int TEXT_VIEW_TYPE = 1;
-//    private static final int ARTICLE_HEADER_VIEW_TYPE = 2;
-    private static final int OUTBRAIN_HEADER_VIEW_TYPE = 3;
+    private static final int ORIGINAL_RECYCLE_VIEW_SIZE = 3;
+    private static final int ARTICLE_HEADER_VIEW_TYPE = 1;
+    private static final int ARTICLE_DETAILS_VIEW_TYPE = 2;
+    private static final int ARTICLE_TEXT_TYPE = 3;
+    private static final int ARTICLE_IMAGE_TYPE = 4;
+    private static final int OUTBRAIN_HEADER_VIEW_TYPE = 5;
 
     private String outbrainWidgetId;
     private String outbrainUrl;
@@ -33,6 +36,44 @@ public class SmartFeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
 
     public void setOutbrainUrl(String outbrainUrl) {
         this.outbrainUrl = outbrainUrl;
+    }
+
+    public class TextItemViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+        public View layout;
+
+        public TextItemViewHolder(View v) {
+            super(v);
+            layout = v;
+            textView = (TextView) v.findViewById(R.id.article_text_view);
+        }
+    }
+
+    public class ImageItemViewHolder extends RecyclerView.ViewHolder {
+        public View layout;
+
+        public ImageItemViewHolder(View v) {
+            super(v);
+            layout = v;
+        }
+    }
+
+    public class HeaderItemViewHolder extends RecyclerView.ViewHolder {
+        public View layout;
+
+        public HeaderItemViewHolder(View v) {
+            super(v);
+            layout = v;
+        }
+    }
+
+    public class DetailItemViewHolder extends RecyclerView.ViewHolder {
+        public View layout;
+
+        public DetailItemViewHolder(View v) {
+            super(v);
+            layout = v;
+        }
     }
 
     // view holder for outbrain header
@@ -55,6 +96,22 @@ public class SmartFeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         View v;
         switch (viewType) {
             // add here more view holders for another view types
+            case ARTICLE_HEADER_VIEW_TYPE:
+                v = inflater.inflate(R.layout.article_header, parent, false);
+                HeaderItemViewHolder ivh = new HeaderItemViewHolder(v);
+                return ivh;
+            case ARTICLE_DETAILS_VIEW_TYPE:
+                v = inflater.inflate(R.layout.article_details, parent, false);
+                DetailItemViewHolder dvh = new DetailItemViewHolder(v);
+                return dvh;
+            case ARTICLE_TEXT_TYPE:
+                v = inflater.inflate(R.layout.article_text, parent, false);
+                TextItemViewHolder tvh = new TextItemViewHolder(v);
+                return tvh;
+            case ARTICLE_IMAGE_TYPE:
+                v = inflater.inflate(R.layout.article_image, parent, false);
+                ImageItemViewHolder imvh = new ImageItemViewHolder(v);
+                return imvh;
             case OUTBRAIN_HEADER_VIEW_TYPE:
                 v = inflater.inflate(R.layout.outbrain_sfeed_header, parent, false);
                 return new OutbrainHeaderViewHolder(v);
@@ -65,9 +122,25 @@ public class SmartFeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        TextItemViewHolder tvh;
+
         switch (position) {
             // add here more view holders for another view types
             case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                tvh = (TextItemViewHolder)holder;
+                tvh.textView.setText(tvh.textView.getContext().getResources().getString(R.string.article_text_1));
+                break;
+            case 3:
+                break;
+            case 4:
+                tvh = (TextItemViewHolder)holder;
+                tvh.textView.setText(tvh.textView.getContext().getResources().getString(R.string.article_text_2));
+                break;
+            case 5:
                 // outbrain header view
 
                 // setting up outbrain header click listener
@@ -96,6 +169,16 @@ public class SmartFeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
     public int getItemViewType(int position) {
         switch (position) {
             case 0:
+                return ARTICLE_HEADER_VIEW_TYPE;
+            case 1:
+                return ARTICLE_DETAILS_VIEW_TYPE;
+            case 2:
+                return ARTICLE_TEXT_TYPE;
+            case 3:
+                return ARTICLE_IMAGE_TYPE;
+            case 4:
+                return ARTICLE_TEXT_TYPE;
+            case 5:
                 return OUTBRAIN_HEADER_VIEW_TYPE;
             default:
                 return this.obSmartFeed.getItemViewType(position, ORIGINAL_RECYCLE_VIEW_SIZE);
